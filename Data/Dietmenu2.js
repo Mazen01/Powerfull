e7be2787e1492d3d84c308d524354eb68fdf1c9a
Mazen01 <bigmaz@live.com>
2022-07-20 22:26:54 +0200

    Add files via upload

---
+++ Data/Dietmenu2.js
@@ -0,0 +1,46 @@
+const Menu2 = {
+    Breakfast: [
+      {
+        id: "2001",
+        item: "1 bowl of Granola Cereal With Dried Fruits",
+      },
+      { id: "2002", item: "Hash Browns" },
+      {
+        id: "2003",
+        item: "Oats with Almond Milk",
+      },
+      { id: "2004", item: "2 scrambled eggs (no butter or oil)" },
+    ],
+    Snack: [
+      { id: "3001", item: "Low-Fat Cheese Sandwich" },
+      {
+        id: "3002",
+        item: "Some nuts (almonds,pistachio nuts,cashew nuts,etc.)",
+      },
+      { id: "3003", item: "1 cookie (low-sugar or sugar-free is better)" },
+      { id: "3004", item: "Baba Genough with a slice of Bread" },
+    ],
+    Lunch: [
+      {
+        id: "4001",
+        item: "Stir-Vegies with Broccoli Soup",
+      },
+      {
+        id: "4002",
+        item: "Any grilled or boiled lean meats (beef,chicken breast,lamb,etc.)",
+      },
+      { id: "4003", item: "Salad That includes (olive-oil, Cucumber, lettuce, nuts)" },
+      { id: "4004", item: "Fruit Smoothie" },
+    ],
+    Dinner: [
+      {
+        id: "5001",
+        item: "Chicken Salad (breast,grilled or boiled ,with fruits or vegetables)",
+      },
+      { id: "5002", item: "Any vegetable soup with no salt added" },
+      { id: "5003", item: "Any grilled or boiled fish or seafood" },
+      { id: "5004", item: "Yogurt (low-fat or low-sugar is better)" },
+    ],
+  };
+  
+  export default Menu2;
